0959090d51ce721715f0b3fb45b6719f3425e67f
SaveKzy <137713062+SaveKzy@users.noreply.github.com>
2024-03-13 15:27:21 -0300

    Create 0313.cpp
    aula 1

---
+++ 0313.cpp
@@ -0,0 +1,63 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  /*
+  
+  int a; // 32 bits (1 sinal, 31 numero)
+  float b; // 32 bits
+  double c; // 64 bits
+  long long int d; // 64 bits
+
+  // 0.1 + 0.2 = 0.3 ?
+
+  char e = 'e'; // 8 bits
+
+  bool f = true; // 1 bit (1 -> true)
+
+  *//*
+
+  int a;
+
+  // scanf("%d", &a); // C
+
+  cin >> a; // C++
+
+  // printf("%d", &a); // C
+
+  cout << a;
+
+  double b = 3.1415926535897932384626;
+
+  cout << fixed << setprecision(2);
+  cout << b;
+
+  // operacoes basicas: + - * / %
+
+  *//*
+
+  int idade;
+
+  cin >> idade;
+
+  // <, >, <=, >=, ==, !=
+  
+  if (idade < 18) {
+    cout << "Menor de idade\n";
+  } else {
+    cout << "Maior de idade\n";
+  }
+
+  if (idade < 18) {}
+  else if (idade < 60) {}
+  else {}
+
+  */
+
+  
+  
+}
+
+// codeforces
+// sublime
